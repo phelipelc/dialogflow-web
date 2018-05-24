ac6d0d6fb8459753908a9ac7198669901ced14e0
phelipelc <phelipelcampos@hotmail.com>
2018-05-24 19:21:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
     app: {
-        token: "9d686a47b1de48bab431e94750d1cd87", // <- enter your token here
+        token: "c11bca43739848fcbba09d24d5ef9b4f", // <- enter your token here
         muted: false, // <- mute microphone by default
         watermark: true // <- show watermark
     },
